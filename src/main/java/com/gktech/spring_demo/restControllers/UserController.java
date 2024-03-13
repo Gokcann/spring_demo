7b09c1a0d02d8e6393ed7ad729453414828fa802
Gokcann <gokcan.akoya@gmail.com>
2024-03-13 23:53:54 +0300

    hasusername control

--- src/main/java/com/gktech/spring_demo/restControllers/UserController.java
+++ src/main/java/com/gktech/spring_demo/restControllers/UserController.java
@@ -2,24 +2,31 @@ package com.gktech.spring_demo.restControllers;
 
 import com.gktech.spring_demo.entity.User;
 import com.gktech.spring_demo.services.serviceImpl.UserServiceImpl;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@AllArgsConstructor
+//lomboka bagli 3 adet constructor yapisi var bunlar;
+//@AllArgsConstructor //parametreli constructor olusturur
+//@NoArgsConstructor //parametresiz constructor olusturur
+@RequiredArgsConstructor //final tanimlarsak
 @RequestMapping("/user")
 public class UserController {
 
-    private UserServiceImpl userService; //construcutor icin NoArgsConstructor ekledik.
+    private final UserServiceImpl userService; //construcutor icin NoArgsConstructor ekledik.
     //Eger lombok constructor eklemek istemezsek final tanimlayip constructor kendimiz olusturmamiz lazim.
     @PostMapping("/save")
     public ResponseEntity<?> addUser(@RequestBody User user) {
         return userService.addUser(user);
     }
+    /*
+    **Mapping Turleri;
+    *
+    @PostMapping //veri kayit (secret)
+    @GetMapping //veri cekme
+    @PutMapping //veri update
+    @DeleteMapping //veri silme
+     */
 
 }
--- src/main/java/com/gktech/spring_demo/services/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/gktech/spring_demo/services/serviceImpl/UserServiceImpl.java
@@ -3,19 +3,23 @@ package com.gktech.spring_demo.services.serviceImpl;
 import com.gktech.spring_demo.entity.User;
 import com.gktech.spring_demo.repositories.UserRepository;
 import com.gktech.spring_demo.services.UserService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-@AllArgsConstructor     //@AutoWired kullanimi tavsiye edilmez
+//@AllArgsConstructor     //@AutoWired kullanimi tavsiye edilmez
+@RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
     //@AutoWired   //denemek icin yazildi
     private final UserRepository userRepository;
     @Override
     public ResponseEntity<?> addUser(User user) {
+        boolean hasUserName = userRepository.existsByUserName(user.getUsername());
+        if (hasUserName) {
+            return new ResponseEntity<>("Already exists user",HttpStatus.IM_USED);
+        }
         userRepository.save(user);
         return new ResponseEntity<>("CreatedUser", HttpStatus.CREATED);
         //ResponseEntity geri donus bicimini tasiyan nesnedir.
